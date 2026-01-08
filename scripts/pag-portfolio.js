af4981cf469a7d164352640e1c25a20df6aec8ea
Lucas Silva <155262753+LucasSilva006@users.noreply.github.com>
2026-01-08 20:07:41 -0300

    Página de portfólio para mobile 95% pronta, eu acho

--- scripts/pag-portfolio.js
+++ scripts/pag-portfolio.js
@@ -1,5 +1,13 @@
-function exibirCardPort() {
+function mostrarMenu() {
+  const nav = document.querySelector("nav");
+  nav.classList.toggle("mostrar");
+}
+
+function exibirCardPort(imgSrc) {
   const modal = document.getElementById("modalProjeto");
+  const img = modal.querySelector("img");
+
+  img.src = imgSrc;
   modal.classList.add("ativa");
   document.body.style.overflow = "hidden";
 }
@@ -8,4 +16,4 @@ function fecharModal() {
   const modal = document.getElementById("modalProjeto");
   modal.classList.remove("ativa");
   document.body.style.overflow = "auto";
-}
+} 
